365d656fb5b7f027148c92f5e5c0f9b9343a177a
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-08-17 14:40:17 +0500

    add assets and lint rules

---
+++ lib/utilities/app_images.dart
@@ -0,0 +1,12 @@
+class AppImages {
+  //
+  // LOGOS
+  //
+  static const String logoBlack = 'assets/logos/logo-black.webp';
+  static const String logoWhite = 'assets/logos/logo-black.webp';
+
+  //
+  // JSONS
+  //
+  static const String computerJSON = 'assets/jsons/computer.json';
+}
